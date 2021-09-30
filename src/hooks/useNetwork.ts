b5f52c73af7d8a7a6c9a8a2163e0d4732d7a9a73
Christian Stuff <christian@daily.co>
2021-09-30 10:40:54 +0200

    implement useNetwork hook

---
+++ src/hooks/useNetwork.ts
@@ -0,0 +1,77 @@
+import {
+  DailyEventObjectNetworkConnectionEvent,
+  DailyEventObjectNetworkQualityEvent,
+  DailyNetworkStats,
+  DailyNetworkTopology,
+} from '@daily-co/daily-js';
+import { useCallback } from 'react';
+import { atom, useRecoilState } from 'recoil';
+
+import { useDaily } from './useDaily';
+import { useDailyEvent } from './useDailyEvent';
+
+interface UseNetworkArgs {
+  onNetworkConnection?(ev: DailyEventObjectNetworkConnectionEvent): void;
+  onNetworkQualityChange?(ev: DailyEventObjectNetworkQualityEvent): void;
+}
+
+const topologyState = atom<DailyNetworkTopology>({
+  key: 'topology',
+  default: 'peer',
+});
+const networkQualityState = atom<DailyNetworkStats['quality']>({
+  key: 'networkQuality',
+  default: 100,
+});
+const networkThresholdState = atom<DailyNetworkStats['threshold']>({
+  key: 'networkThreshold',
+  default: 'good',
+});
+
+export const useNetwork = ({
+  onNetworkConnection,
+  onNetworkQualityChange,
+}: UseNetworkArgs = {}) => {
+  const daily = useDaily();
+
+  const [topology, setTopology] = useRecoilState(topologyState);
+  const [quality, setQuality] = useRecoilState(networkQualityState);
+  const [threshold, setThreshold] = useRecoilState(networkThresholdState);
+
+  const handleNetworkConnection = useCallback(
+    (ev: DailyEventObjectNetworkConnectionEvent) => {
+      switch (ev.event) {
+        case 'connected':
+          if (ev.type === 'peer-to-peer') setTopology('peer');
+          if (ev.type === 'sfu') setTopology('sfu');
+          break;
+      }
+      onNetworkConnection?.(ev);
+    },
+    [onNetworkConnection]
+  );
+
+  const handleNetworkQualityChange = useCallback(
+    (ev: DailyEventObjectNetworkQualityEvent) => {
+      setQuality((q) => (q !== ev.quality ? ev.quality : q));
+      setThreshold((t) => (t !== ev.threshold ? ev.threshold : t));
+      onNetworkQualityChange?.(ev);
+    },
+    [onNetworkQualityChange]
+  );
+
+  useDailyEvent('network-connection', handleNetworkConnection);
+  useDailyEvent('network-quality-change', handleNetworkQualityChange);
+
+  const getStats = useCallback(async () => {
+    const newStats = await daily?.getNetworkStats();
+    return newStats?.stats;
+  }, [daily]);
+
+  return {
+    getStats,
+    quality,
+    threshold,
+    topology,
+  };
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { DailyProvider } from './DailyProvider';
 export { useDaily } from './hooks/useDaily';
 export { useDailyEvent } from './hooks/useDailyEvent';
+export { useNetwork } from './hooks/useNetwork';
 export { useWaitingParticipants } from './hooks/useWaitingParticipants';
