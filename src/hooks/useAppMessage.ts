c46705268063ca07631307ac366bb2a5dae67742
Christian Stuff <christian@daily.co>
2021-10-11 13:10:50 +0200

    implement useAppMessage

---
+++ src/hooks/useAppMessage.ts
@@ -0,0 +1,26 @@
+import { DailyEventObjectAppMessage } from '@daily-co/daily-js';
+import { useCallback } from 'react';
+
+import { useDaily, useDailyEvent } from '..';
+
+interface UseAppMessageArgs<Data> {
+  onAppMessage?(ev: DailyEventObjectAppMessage<Data>): void;
+}
+
+export const useAppMessage = <Data = any>({
+  onAppMessage,
+}: UseAppMessageArgs<Data>) => {
+  const daily = useDaily();
+
+  const sendAppMessage = useCallback(
+    (data: Data, to: string = '*') => {
+      if (!daily) return;
+      daily.sendAppMessage(data, to);
+    },
+    [daily]
+  );
+
+  useDailyEvent('app-message', onAppMessage);
+
+  return sendAppMessage;
+};
