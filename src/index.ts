4f573a8070b4726a44fde8083c015e20055b0539
Christian Stuff <christianstuff90@gmail.com>
2021-11-01 17:17:27 +0100

    Revert "ENG-2601 Implement useAppMessage"

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { DailyProvider } from './DailyProvider';
-export { useAppMessage } from './hooks/useAppMessage';
 export { useDaily } from './hooks/useDaily';
 export { useDailyEvent } from './hooks/useDailyEvent';
 export { useNetwork } from './hooks/useNetwork';
