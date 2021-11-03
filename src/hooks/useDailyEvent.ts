fc48ce6731dcc389703bdc4276af3541912951c7
Christian Stuff <christian@daily.co>
2021-11-03 11:44:24 +0100

    improve daily event cleanups

--- src/hooks/useDailyEvent.ts
+++ src/hooks/useDailyEvent.ts
@@ -5,11 +5,11 @@ import { DailyEventContext } from '../DailyProvider';
 
 type EventCallback = (event?: DailyEventObject) => void;
 
-export const useDailyEvent = (ev: DailyEvent, callback?: EventCallback) => {
+export const useDailyEvent = (ev: DailyEvent, callback: EventCallback) => {
   const { on, off } = useContext(DailyEventContext);
 
   useEffect(() => {
-    if (!ev || !callback) return;
+    if (!ev) return;
     on(ev, callback);
     return () => {
       off(ev, callback);
