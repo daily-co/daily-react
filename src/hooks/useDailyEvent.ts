25b362dfcffeb9fd95b35c53568805dff2ae2b73
Christian Stuff <christian@daily.co>
2021-11-12 11:42:25 +0100

    add safe guard against re-render loop in useDailyEvent

--- src/hooks/useDailyEvent.ts
+++ src/hooks/useDailyEvent.ts
@@ -1,5 +1,5 @@
 import { DailyEvent, DailyEventObject } from '@daily-co/daily-js';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 
 import { DailyEventContext } from '../DailyProvider';
 
@@ -7,12 +7,32 @@ type EventCallback = (event?: DailyEventObject) => void;
 
 export const useDailyEvent = (ev: DailyEvent, callback: EventCallback) => {
   const { on, off } = useContext(DailyEventContext);
+  const [isBlocked, setIsBlocked] = useState(false);
+  const reassignCount = useRef<number>(0);
 
   useEffect(() => {
-    if (!ev) return;
+    if (!ev || isBlocked) return;
+    /**
+     * Check if callback has been reassigned often enough without hitting the 50ms timeout.
+     */
+    if (reassignCount.current > 100) {
+      console.error(
+        `useDailyEvent called with potentially non-memoized event callback.
+        Memoize using useCallback to avoid re-render loop or reduce the amount of state transitions the callback depends on.
+        Passed callback for '${ev}' event is NOT registered.`,
+        callback
+      );
+      setIsBlocked(true);
+      return;
+    }
+    reassignCount.current++;
+    const timeout = setTimeout(() => {
+      reassignCount.current = 0;
+    }, 50);
     on(ev, callback);
     return () => {
+      clearTimeout(timeout);
       off(ev, callback);
     };
-  }, [callback, ev, off, on]);
+  }, [callback, ev, isBlocked, off, on]);
 };
