db42b0e59f86f0548e69be4fa3aab790616b098e
Christian Stuff <christian@daily.co>
2022-02-17 10:56:57 +0100

    add comments

--- src/hooks/useRecording.ts
+++ src/hooks/useRecording.ts
@@ -26,14 +26,44 @@ interface UseRecordingArgs {
 }
 
 interface RecordingState {
+  /**
+   * Determines wether an error occurred during the last recording attempt.
+   */
   error?: boolean;
+  /**
+   * Determines wether the local participant is being recorded, based on the recording settings.
+   */
   isLocalParticipantRecorded: boolean;
+  /**
+   * Determines wether a recording is currently running or not.
+   */
   isRecording: boolean;
+  /**
+   * Contains the last applied cloud recording layout config.
+   */
   layout?: DailyStreamingLayoutConfig;
+  /**
+   * Determines wether the recording is running locally.
+   * See [enable_recording](https://docs.daily.co/reference/rest-api/rooms/config#enable_recording).
+   */
   local?: boolean;
+  /**
+   * Contains the recording id.
+   */
   recordingId?: string;
+  /**
+   * Contains the date when the 'recording-started' event was received.
+   * This doesn't necessarily match the date the recording was actually started.
+   */
   recordingStartedDate?: Date;
+  /**
+   * Contains the session_id of the participant who started the recording.
+   */
   startedBy?: string;
+  /**
+   * Contains the recording type.
+   * See [enable_recording](https://docs.daily.co/reference/rest-api/rooms/config#enable_recording).
+   */
   type?: string;
 }
 
@@ -162,6 +192,9 @@ export const useRecording = ({
     )
   );
 
+  /**
+   * Starts the recording with the given optional options.
+   */
   const startRecording = useCallback(
     (options?: DailyStreamingOptions) => {
       if (!daily) return;
@@ -170,11 +203,17 @@ export const useRecording = ({
     [daily]
   );
 
+  /**
+   * Stops a recording.
+   */
   const stopRecording = useCallback(() => {
     if (!daily) return;
     daily.stopRecording();
   }, [daily]);
 
+  /**
+   * Updates a running recording's layout configuration.
+   */
   const updateRecording = useCallback(
     (options: { layout?: DailyStreamingLayoutConfig | undefined }) => {
       if (!daily) return;
