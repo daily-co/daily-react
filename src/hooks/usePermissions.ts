4dff18f7c6bc338536a1d1684481fa931b415625
Christian Stuff <christian@daily.co>
2023-05-22 11:38:34 +0200

    imlement usePermissions

---
+++ src/hooks/usePermissions.ts
@@ -0,0 +1,43 @@
+import { useLocalSessionId } from './useLocalSessionId';
+import { useParticipantProperty } from './useParticipantProperty';
+
+export const usePermissions = () => {
+  const localSessionId = useLocalSessionId();
+  const permissions = useParticipantProperty(localSessionId, 'permissions');
+
+  const canSendAudio =
+    typeof permissions?.canSend === 'boolean'
+      ? permissions?.canSend
+      : permissions?.canSend.has('audio');
+  const canSendVideo =
+    typeof permissions?.canSend === 'boolean'
+      ? permissions?.canSend
+      : permissions?.canSend.has('video');
+  const canSendCustomAudio =
+    typeof permissions?.canSend === 'boolean'
+      ? permissions?.canSend
+      : permissions?.canSend.has('customAudio');
+  const canSendCustomVideo =
+    typeof permissions?.canSend === 'boolean'
+      ? permissions?.canSend
+      : permissions?.canSend.has('customVideo');
+  const canSendScreenAudio =
+    typeof permissions?.canSend === 'boolean'
+      ? permissions?.canSend
+      : permissions?.canSend.has('screenAudio');
+  const canSendScreenVideo =
+    typeof permissions?.canSend === 'boolean'
+      ? permissions?.canSend
+      : permissions?.canSend.has('screenVideo');
+
+  return {
+    canSendAudio,
+    canSendCustomAudio,
+    canSendCustomVideo,
+    canSendScreenAudio,
+    canSendScreenVideo,
+    canSendVideo,
+    hasPresence: permissions?.hasPresence,
+    permissions,
+  };
+};
