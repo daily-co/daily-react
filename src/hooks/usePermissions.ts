edfbf69e4c831f7ee77df1490658d3a92b4556d6
Harshith Pabbati <pabbatiharshith@gmail.com>
2023-06-13 16:51:14 +0530

    Update north-star/packages/daily-react/src/hooks/usePermissions.ts
    Co-authored-by: Christian Stuff <christian@daily.co>

--- src/hooks/usePermissions.ts
+++ src/hooks/usePermissions.ts
@@ -1,6 +1,11 @@
 import { useLocalSessionId } from './useLocalSessionId';
 import { useParticipantProperty } from './useParticipantProperty';
 
+/**
+ * Returns parsed permissions for a given participant.
+ * In case no `sessionId` is passed, the hook returns permissions for the local participant.
+ * @param sessionId The participant's session_id (optional)
+ */
 export const usePermissions = (sessionId?: string) => {
   const localSessionId = useLocalSessionId();
   const permissions = useParticipantProperty(
