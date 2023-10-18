b1c1ab231c5a01afd08bd03cec1aa3aa7e0e9f26
Christian Stuff <christian@daily.co>
2023-10-18 11:10:25 +0200

    move screen shares to recoil selector
    This has a bunch of net-positive results:
    - No need to render DailyScreenShares as a parent component -> smaller bundle size
    - No need to setup a Recoil transaction observer, because scree share state will be automatically re-calculated when one of the `screens` participant ids or their properties of interest (local, tracks.screenAudio, tracks.screenVideo)  change
    - Simplifies the entire useScreenShare hook

--- src/hooks/useScreenShare.ts
+++ src/hooks/useScreenShare.ts
@@ -4,11 +4,14 @@ import {
   DailyTrackState,
 } from '@daily-co/daily-js';
 import { useCallback } from 'react';
+import { selector, useRecoilValue } from 'recoil';
 
-import { useScreenSharesContext } from '../DailyScreenShares';
+import { RECOIL_PREFIX } from '../lib/constants';
 import { Reconstruct } from '../types/Reconstruct';
 import { useDaily } from './useDaily';
 import { useDailyEvent } from './useDailyEvent';
+import { participantIdsFilteredAndSortedState } from './useParticipantIds';
+import { participantPropertyState } from './useParticipantProperty';
 
 export interface ScreenShare {
   local: boolean;
@@ -17,6 +20,28 @@ export interface ScreenShare {
   screenId: string;
   session_id: string;
 }
+
+const screenSharesState = selector({
+  key: RECOIL_PREFIX + 'screen-shares',
+  get: ({ get }) => {
+    const screenIds = get(
+      participantIdsFilteredAndSortedState({ filter: 'screen', sort: null })
+    );
+    return screenIds.map<ScreenShare>((id) => {
+      return {
+        local: get(participantPropertyState({ id, property: 'local' })),
+        screenAudio: get(
+          participantPropertyState({ id, property: 'tracks.screenAudio' })
+        ),
+        screenVideo: get(
+          participantPropertyState({ id, property: 'tracks.screenVideo' })
+        ),
+        screenId: `${id}-screen`,
+        session_id: id,
+      };
+    });
+  },
+});
 
 type DailyEventObjectScreenShareError = Reconstruct<
   DailyEventObjectNonFatalError,
@@ -79,7 +104,7 @@ export const useScreenShare = ({
     )
   );
 
-  const { screens } = useScreenSharesContext();
+  const screens = useRecoilValue(screenSharesState);
 
   return {
     isSharingScreen: screens.some((s) => s.local),
