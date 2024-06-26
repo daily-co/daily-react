11061e1286883c845e201d3af8c7ed301cb5cab8
Christian Stuff <christian@daily.co>
2024-06-26 08:50:05 +0200

    fix: add a circular reference check for getPaths
    Co-Authored-By: Stewart McGown <stewart@mcgown.dev>

--- src/utils/getParticipantPaths.ts
+++ src/utils/getParticipantPaths.ts
@@ -4,18 +4,26 @@ import { Paths } from '../types/paths';
 /**
  * Returns all property paths for an object.
  */
-const getPaths = (o: Record<any, any>, currentPath = ''): string[] => {
-  if (typeof o !== 'object' || o === null) {
+const getPaths = (
+  o: Record<any, any>,
+  currentPath = '',
+  visited = new Set()
+): string[] => {
+  if (typeof o !== 'object' || o === null || visited.has(o)) {
     return [currentPath];
   }
+
+  visited.add(o);
 
   const paths = [];
   for (const key in o) {
     if (Object.prototype.hasOwnProperty.call(o, key)) {
       const newPath = currentPath ? `${currentPath}.${key}` : key;
-      paths.push(newPath, ...getPaths(o[key], newPath));
+      paths.push(newPath, ...getPaths(o[key], newPath, visited));
     }
   }
+
+  visited.delete(o);
 
   return paths;
 };
