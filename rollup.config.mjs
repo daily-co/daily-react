4b873f9a784a0bc16e24281116abce1f926eed12
James Hush <james@daily.co>
2025-08-15 13:01:07 +0800

    Update rollup.config
    Switch terser plugin to maintained version
    
    build: add unminifed index.esm.js
    
    Fix file path names
    
    Change size

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,49 +2,77 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import { babel } from '@rollup/plugin-babel';
 import copy from 'rollup-plugin-copy';
 import fs from 'fs-extra';
 
 const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: packageJson.main,
+export default [
+  // ESM build (unminified)
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    plugins: [
+      peerDepsExternal(),
+      resolve({
+        browser: true,
+        preferBuiltins: false,
+      }),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        exclude: ['**/*.test.*', '**/*.stories.*'],
+      }),
+      babel({
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**',
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        presets: [
+          ['@babel/preset-env', { exclude: ['transform-regenerator'] }],
+        ],
+      }),
+      copy({
+        targets: [{ src: 'src/types', dest: 'dist' }],
+      }),
+    ],
+    external: ['@daily-co/daily-js', 'jotai', 'react', 'react-dom'],
+  },
+  // CJS build (minified)
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.js',
       format: 'cjs',
       sourcemap: true,
       exports: 'named',
     },
-    {
-      file: packageJson.module,
-      format: 'esm',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve({
-      browser: true,
-      preferBuiltins: false,
-    }),
-    commonjs(),
-    typescript({
-      tsconfig: './tsconfig.json',
-      exclude: ['**/*.test.*', '**/*.stories.*'],
-    }),
-    babel({
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**',
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      presets: [['@babel/preset-env', { exclude: ['transform-regenerator'] }]],
-    }),
-    terser(),
-    copy({
-      targets: [{ src: 'src/types', dest: 'dist' }],
-    }),
-  ],
-  external: ['@daily-co/daily-js', 'jotai', 'react', 'react-dom'],
-};
+    plugins: [
+      peerDepsExternal(),
+      resolve({
+        browser: true,
+        preferBuiltins: false,
+      }),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        exclude: ['**/*.test.*', '**/*.stories.*'],
+      }),
+      babel({
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**',
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        presets: [
+          ['@babel/preset-env', { exclude: ['transform-regenerator'] }],
+        ],
+      }),
+      terser(),
+    ],
+    external: ['@daily-co/daily-js', 'jotai', 'react', 'react-dom'],
+  },
+];
