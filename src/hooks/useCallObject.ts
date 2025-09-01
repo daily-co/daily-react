cc836b053c0248aa15ca578a6b9d93bc108173a9
Christian Stuff <christian@daily.co>
2025-09-01 07:40:20 +0200

    handle WebRTC error in daily-react

--- src/hooks/useCallObject.ts
+++ src/hooks/useCallObject.ts
@@ -4,6 +4,8 @@ type CallObjectProps = Omit<Props, 'parentEl'>;
 
 /**
  * Helper hook to maintain custom callObject instances in React codebases.
+ * This hook will throw an error if the call object cannot be created in strict mode,
+ * which is true when the browser doesn't support WebRTC.
  */
 export const useCallObject = (props: CallObjectProps) =>
   useCallInstance('callObject', props);
